38aa477365dfdaf9597c1d7507f66cfbfb5db20d
Vamberto Rocha JR <vambertojr@gmail.com>
2021-03-28 21:30:21 -0300

    cadastro de usuários

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,17 @@
+import 'package:pgia7310/src/user.dart';
+
+import '';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Vamberto',
+    email: 'teste@teste.com',
+  ),
+  '2': const User(
+  id: '2',
+  name: 'Thais',
+  email: 'teste2@teste2.com',
+  ),
+
+};
