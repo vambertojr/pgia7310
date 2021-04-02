539216ba203f9a6066241a75d0f06a85e24211ae
Vamberto Rocha JR <vambertojr@gmail.com>
2021-04-02 20:28:25 -0300

    Implementação da primeira tela

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -6,11 +6,18 @@ class FirstScreen extends StatelessWidget {
     return Material(
       color: Colors.cyan,
       child: Center(
+
         child: Text(
           "Fluteando",
           textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 40.0),
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 40.0,
+              fontFamily: 'Oxygen',
+              fontWeight: FontWeight.normal),
+
         ),
+
       ),
 
     );
@@ -18,5 +25,15 @@ class FirstScreen extends StatelessWidget {
 
   }
 
+
+}
+
+class DiseaseImage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    AssetImage diseaseAsset = AssetImage('images/imagem1.png');
+    Image image = Image(image: diseaseAsset, width: 400.0, height: 400.0,);
+    return Container(child: image);
+  }
 
 }
